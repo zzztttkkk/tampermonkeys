533872e57de5da8d458f0a7d36471ab3f84c6b97
ztk <ztkisalreadytaken@gmail.com>
2020-12-16 03:43:16 +0800

    Update remove_solidot_comments.js

--- remove_solidot_comments.js
+++ remove_solidot_comments.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.solidot.org/
+// @match        https://www.solidot.org/*
 // @grant        none
 // ==/UserScript==
 
