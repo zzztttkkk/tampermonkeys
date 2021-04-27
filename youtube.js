2d9756d9039974fa76a3de95887fc4972628d771
ztk <ztkisalreadytaken@gmail.com>
2021-04-27 22:48:49 +0800

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,2 +1,12 @@
+// ==UserScript==
+// @name         remove_solidot_comments.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.youtube.*/*
+// @grant        none
+// ==/UserScript==
+
 document.querySelector("a.ytp-next-button.ytp-button").remove();
 window.onload = function(){document.querySelector("a.ytp-next-button.ytp-button").remove();}
