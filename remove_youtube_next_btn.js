b6bc63341db2b6c8dacd174ed780beb55bfe5305
ztk <ztkisalreadytaken@gmail.com>
2021-04-27 22:58:03 +0800

    Update remove_youtube_next_btn.js

--- remove_youtube_next_btn.js
+++ remove_youtube_next_btn.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         remove_solidot_comments.js
+// @name         remove_youtube_next_btn.js
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
